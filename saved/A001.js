55a49299bcedbfad557b437d25d991f4e0cdba5e
VerneJules <vernejules@anivide.com>
2020-03-09 14:31:17 +0800

    Add files via upload

---
+++ saved/A001.js
@@ -0,0 +1,12 @@
+var search_contacts = function(){
+    //write code here for when the value of search filter for contacts is changed
+}
+
+var contact_selected = function(){
+    //write code here for when a contact is selected
+}
+
+var search_projects = function(){
+    //write code here for when the value of search filter for projects is changed
+    }
+}
